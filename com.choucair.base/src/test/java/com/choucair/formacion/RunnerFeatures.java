955cc75b9f8505615d1080616c4f6f25ded37ec2
lcortesm <lcortesm@CHBOGML-122949.corp.choucairtesting.local>
2019-11-18 14:20:24 -0500

    Primer commit

--- com.choucair.base/src/test/java/com/choucair/formacion/RunnerFeatures.java
+++ com.choucair.base/src/test/java/com/choucair/formacion/RunnerFeatures.java
@@ -7,8 +7,8 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
 //@CucumberOptions(features ="src/test/resources/features/")
-@CucumberOptions(features ="src/test/resources/features/ColorLib/")
-//@CucumberOptions(features ="src/test/resources/features/ColorLib/nombre.feature")
+//@CucumberOptions(features ="src/test/resources/features/ColorLib/")
+@CucumberOptions(features ="src/test/resources/features/ColorLib/nombre.feature")
 public class RunnerFeatures {
 
 }
